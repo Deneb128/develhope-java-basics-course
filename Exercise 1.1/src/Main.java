0d6ae3dceb35fcd09e40c3ef97d0126b19651ee0
Roberto Inconis <robertoinconis1607@gmail.com>
2023-10-10 12:09:30 +0200

    -gitignore update for intelliJ files and source code of exercise

---
+++ Exercise 1.1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        final int base = 5;
+        final int height = 7;
+
+
+
+        System.out.println("Area of the triangle of base " + base + "and height " + height + " is " + (float)(base*height)/2);
+    }
+}
