44b2a6e21c8f38dacdc36d14c8de9d6a2054891c
Roberto Inconis <robertoinconis1607@gmail.com>
2023-10-11 12:30:22 +0200

    Exercise 1.4

---
+++ Exercise 1.4/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        final char c1 = 'f';
+        final char c2 = 'm';
+        System.out.println(c1 + c2); //converted implicitly
+    }
+}
