8e5eb388be59d8cb110a1d60c7ddbc87a0ee4faa
Roberto Inconis <robertoinconis1607@gmail.com>
2023-10-11 12:34:31 +0200

    exercise 1.5

---
+++ Exercise 1.5/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        final int x = 5;
+        final int y = 7;
+
+        System.out.println("x + y -> " + Sum(x,y));
+    }
+
+    public static int Sum(int x, int y)
+    {
+        return x + y;
+    }
+}
