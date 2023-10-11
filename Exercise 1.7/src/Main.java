c140777453b755d6807cbe760d6bb92f9d5af3d2
Roberto Inconis <robertoinconis1607@gmail.com>
2023-10-11 12:49:19 +0200

    exercise 1.7

---
+++ Exercise 1.7/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        final double x = 1.0;
+        final double y = 2.0;
+        final double z = 4.0;
+        final double avg = (x + y +z) / 3;
+        System.out.println("avg between (" + x + "-" + y + "-" + z + ") -> " + avg);
+    }
+}
