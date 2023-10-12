e711c817576d949287590f7a7f6ba31b03e1fa74
Roberto Inconis <robertoinconis1607@gmail.com>
2023-10-12 18:19:37 +0200

    exercise 1.19

---
+++ Exercise 1.19/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        final int start = 7;
+        final int counter = 9;
+        for(int i = 0; i < counter; ++i)
+        {
+            System.out.println(start-i);
+        }
+    }
+}
