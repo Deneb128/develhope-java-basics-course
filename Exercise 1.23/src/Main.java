9cc4d6878acb53ab1ffdcc3dd850c38fec33f0fc
Roberto Inconis <robertoinconis1607@gmail.com>
2023-10-15 19:03:31 +0200

    exercise 1.23

---
+++ Exercise 1.23/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        char[] myArr = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+        Random r = new Random();
+        
+        for(int i = 0; i < myArr.length; ++i){
+            myArr[i] = (char)(r.nextInt(26) + 'a');
+        }
+
+        Boolean found = false;
+        for(int i = 0; i < myArr.length; ++i)
+        {
+            if(myArr[i] == 'a') {
+                found = true;
+                System.out.println("Found 'a' at index: " + i);
+            }
+        }
+        if(!found){
+            System.out.println("No 'a' found");
+        }
+
+    }
+}
