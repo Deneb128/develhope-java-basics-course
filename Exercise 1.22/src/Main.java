dcffbcbaf3ffa07943d0d4eca2b04f5b55d3fc25
Roberto Inconis <robertoinconis1607@gmail.com>
2023-10-15 18:53:44 +0200

    exercise 1.22

---
+++ Exercise 1.22/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int[] myArr = {0,0,0,0,0,0,0,0,0,0};
+        int sum = 0;
+        for(int i = 1; i <= 10; ++i)
+        {
+            sum =+ i;
+            myArr[i-1] = i;
+        }
+
+        for(int value : myArr){
+            System.out.println(value);
+        }
+        System.out.println("Sum of array: " + sum);
+
+    }
+}
