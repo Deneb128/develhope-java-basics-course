643dfa5f633b81756fe8d451de54fbfb18401de0
Roberto Inconis <robertoinconis1607@gmail.com>
2023-10-25 15:21:26 +0200

    exercise 3.5

---
+++ Exercise 3.5/src/Form.java
@@ -0,0 +1,3 @@
+interface Form {
+    double calculateArea();
+}
---
+++ Exercise 3.5/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var form1 = new Rectangle(5,10);
+        var form2 = new Triangle(5,10);
+        form1.printArea();
+        form2.printArea();
+    }
+}
