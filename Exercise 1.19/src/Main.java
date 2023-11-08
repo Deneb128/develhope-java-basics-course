8d0dcc959237f22f92a8ca9da35aa0163adaff76
Roberto Inconis <robertoinconis1607@gmail.com>
2023-11-08 20:22:37 +0100

    exercise with method

--- Exercise 1.19/src/Main.java
+++ Exercise 1.19/src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         final int start = 7;
         final int counter = 9;
+        printReverse(start,counter);
+    }
+
+    public static void printReverse(int start, int counter) {
         for(int i = 0; i < counter; ++i)
         {
             System.out.println(start-i);
