47d99b65d7ef8676efbc51a6a1febc0169261892
Roberto Inconis <robertoinconis1607@gmail.com>
2023-11-08 20:23:56 +0100

    exercise with method

--- Exercise 1.20/src/Main.java
+++ Exercise 1.20/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         final int limit = 10;
-        for(int i = 0; i < limit; ++i )
-        {
-            if(i == 5){
+        printTo(limit);
+    }
+
+    public static void printTo(int limit) {
+        for (int i = 0; i < limit; ++i) {
+            if (i == 5) {
                 break;
             }
             System.out.println(i);
