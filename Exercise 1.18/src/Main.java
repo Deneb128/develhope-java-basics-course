2b897218dcadab7d7d7c08598c0d49d611f993b8
Roberto Inconis <robertoinconis1607@gmail.com>
2023-11-08 14:27:25 +0100

    exercise with method(s)

--- Exercise 1.18/src/Main.java
+++ Exercise 1.18/src/Main.java
@@ -2,8 +2,22 @@ public class Main {
     public static void main(String[] args) {
         final int number = 7;
         System.out.println("Multiplication table of " + number + " is:");
+        printMultiplicationTable(number);
+        System.out.println(multiplicationTableAsString(number));
+
+    }
+
+    public static void printMultiplicationTable(int n) {
         for(int i = 0; i <= 10; ++i){
-            System.out.println(i + "*" + number + " = " + i*number);
+            System.out.println(i + "*" + n + " = " + i*n);
         }
+    }
+
+    public static String multiplicationTableAsString(int n) {
+        String multiplicationTable = "";
+        for(int i = 0; i <= 10; ++i){
+            multiplicationTable += i + "*" + n + " = " + i*n + "\n";
+        }
+        return multiplicationTable;
     }
 }
